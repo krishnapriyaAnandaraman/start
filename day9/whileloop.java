09bab5d38711d344233914e32d8fc1a6a175b41d
krishnapriyaAnandaraman <krishnapriyaanandaraman@gmail.com>
2025-02-19 22:17:18 +0530

    Add files via upload

---
+++ day9/whileloop.java
@@ -0,0 +1,11 @@
+public class whileloop { 
+    public static void main(String[] args) {
+        int N = 28;  
+        int i = 1;  
+
+        while (i <= N) {
+            System.out.println(i);
+            i++;  
+    }
+}
+}
